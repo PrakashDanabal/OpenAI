666eb6d597deb3de8d57c71cab505a2bd45043f6
Prakash <prakashdanabal@gmail.com>
2024-01-30 16:10:24 +0530

    www

--- app.py
+++ app.py
@@ -29,45 +29,95 @@ import sqlalchemy
 #
 app=Flask(__name__)
 
-def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
-    """Initializes a Unix socket connection pool for a Cloud SQL instance of MySQL."""
+# def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
+#     """Initializes a Unix socket connection pool for a Cloud SQL instance of MySQL."""
+#     # Note: Saving credentials in environment variables is convenient, but not
+#     # secure - consider a more secure solution such as
+#     # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+#     # keep secrets safe.
+#     db_user = 'sadmin'  # e.g. 'my-database-user'
+#     db_pass = '10231993'  # e.g. 'my-database-password'
+#     db_name = 'mdata'  # e.g. 'my-database'
+#     unix_socket_path = 'adding-and-q-281-3662a64f:us-central1:flaskdb'  # e.g. '/cloudsql/project:region:instance'
+#
+#     pool = sqlalchemy.create_engine(
+#         # Equivalent URL:
+#         # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+#         sqlalchemy.engine.url.URL.create(
+#             drivername="mysql+pymysql",
+#             username=db_user,
+#             password=db_pass,
+#             database=db_name,
+#             query={"unix_socket": unix_socket_path},
+#         ),
+#         # ...
+#     )
+#     return pool
+
+
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of SQL Server.
+
+    Uses the Cloud SQL Python Connector package.
+    """
     # Note: Saving credentials in environment variables is convenient, but not
     # secure - consider a more secure solution such as
     # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
     # keep secrets safe.
-    db_user = 'sadmin'  # e.g. 'my-database-user'
-    db_pass = '10231993'  # e.g. 'my-database-password'
+
+    instance_connection_name = 'adding-and-q-281-3662a64f:us-central1:flaskdb'  # e.g. 'project:region:instance'
+    db_user = 'sadmin'  # e.g. 'my-db-user'
+    db_pass = '10231993'  # e.g. 'my-db-password'
     db_name = 'mdata'  # e.g. 'my-database'
-    unix_socket_path = 'adding-and-q-281-3662a64f:us-central1:flaskdb'  # e.g. '/cloudsql/project:region:instance'
+
+    ip_type = IPTypes.PUBLIC
+
+    connector = Connector(ip_type)
+
+    connect_args = {}
+    # If your SQL Server instance requires SSL, you need to download the CA
+    # certificate for your instance and include cafile={path to downloaded
+    # certificate} and validate_host=False. This is a workaround for a known issue.
+    if os.environ.get("DB_ROOT_CERT"):  # e.g. '/path/to/my/server-ca.pem'
+        connect_args = {
+            "cafile": os.environ["DB_ROOT_CERT"],
+            "validate_host": False,
+        }
+
+    def getconn() -> pytds.Connection:
+        conn = connector.connect(
+            instance_connection_name,
+            "pytds",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            **connect_args
+        )
+        return conn
 
     pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,
-            password=db_pass,
-            database=db_name,
-            query={"unix_socket": unix_socket_path},
-        ),
+        "mssql+pytds://",
+        creator=getconn,
         # ...
     )
     return pool
 
-db_user = 'sadmin'
-db_password = '10231993'
-db_name = 'mdata'
-db_socket_dir = '/cloudsql'
-cloud_sql_connection_name = 'adding-and-q-281-3662a64f:us-central1:flaskdb'
+# db_user = 'sadmin'
+# db_password = '10231993'
+# db_name = 'mdata'
+# db_socket_dir = '/cloudsql'
+# cloud_sql_connection_name = 'adding-and-q-281-3662a64f:us-central1:flaskdb'
+#
+# # Configure the SQLAlchemy URI for Google Cloud SQL
+# db_uri = f"mssql://{db_user}:{db_password}@/{db_name}?unix_socket=/cloudsql/{cloud_sql_connection_name}"
+# app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# # db = SQLAlchemy(app)
+# db = connect_unix_socket()
 
-# Configure the SQLAlchemy URI for Google Cloud SQL
-db_uri = f"mssql://{db_user}:{db_password}@/{db_name}?unix_socket=/cloudsql/{cloud_sql_connection_name}"
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-db = connect_unix_socket()
-
+db=connect_with_connector()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
