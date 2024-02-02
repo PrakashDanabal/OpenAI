33066f76888b04e82e36c110d58c94509bcc1f1c
Prakash <prakashdanabal@gmail.com>
2024-02-02 15:22:19 +0530

    this is new edit

--- views.py
+++ views.py
@@ -1,17 +1,43 @@
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from google.cloud.sql.connector import Connector
 
+#there is no hope
+### hope it works
 db = SQLAlchemy()
+
+INSTANCE_CONNECTION_NAME='adding-and-q-281-800e631e:us-central1:flaskdb'
+DB_USER='sqlserver'
+DB_PASS='10231993'
+DB_NAME='mdata'
+
+
+
+def getconn():
+    conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "pytds",
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME
+    )
+    return conn
+
 
 def create_app():
     app = Flask(__name__)
 
     # Flask-SQLAlchemy settings
-    app.config['SQLALCHEMY_DATABASE_URI']= "mssql://sa:10231993@localhost:49993/MData_PMO?driver=SQL+Server+Native+Client+11.0"  # File-based SQL database
-    app.config['SQLALCHEMY_BINDS'] = {
-        'random': "mssql://sa:10231993@localhost:49993/MData_PMO?driver=SQL+Server+Native+Client+11.0"
-    }
+    # app.config['SQLALCHEMY_DATABASE_URI']= "mssql://sa:10231993@localhost:49993/MData_PMO?driver=SQL+Server+Native+Client+11.0"  # File-based SQL database
+    app.config['SQLALCHEMY_DATABASE_URI']= getconn()
+    # app.config['SQLALCHEMY_BINDS'] = {
+    #     'random': "mssql://sqlserver:10231993@localhost:49993/MData_PMO?driver=SQL+Server+Native+Client+11.0"
+    # }
+    # app.config['SQLALCHEMY_DATABASE_URI']= "mssql://sa:10231993@localhost:49993/MData_PMO?driver=SQL+Server+Native+Client+11.0"  # File-based SQL database
+    # app.config['SQLALCHEMY_BINDS'] = {
+    #     'random': "mssql://sa:10231993@localhost:49993/MData_PMO?driver=SQL+Server+Native+Client+11.0"
+    # }
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids SQLAlchemy warning
     db.init_app(app)
     with app.app_context():
