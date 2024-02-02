b7f16545ee8501c86bf23c586b6dc75121f87964
Prakash <prakashdanabal@gmail.com>
2024-02-02 17:20:35 +0530

    ssss this is new

--- app.py
+++ app.py
@@ -1,99 +1,99 @@
-from flask import Flask
+# imports
+from flask import Flask, request, make_response
 from flask_sqlalchemy import SQLAlchemy
-# import main
-from google.cloud.sql.connector import Connector, IPTypes
-import pytds
-import sqlalchemy
+
+# initializing Flask app
+app = Flask(__name__)
+
+# Google Cloud SQL (change this accordingly)
+PASSWORD = "10231993"
+PUBLIC_IP_ADDRESS = "34.133.186.200"
+DBNAME = "mdata"
+PROJECT_ID = "adding-and-q-281-6357138b"
+INSTANCE_NAME = "adding-and-q-281-6357138b:us-central1:flaskdb"
+
+# configuration
+# app.config["SECRET_KEY"] = "yoursecretkey"
 
 
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = f"mssql+pytds://sadmin:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
 
 
-app=Flask(__name__)
-
-def connect_with_connector() -> sqlalchemy.engine.base.Engine:
-    """
-    Initializes a connection pool for a Cloud SQL instance of SQL Server.
-
-    Uses the Cloud SQL Python Connector package.
-    """
-    # Note: Saving credentials in environment variables is convenient, but not
-    # secure - consider a more secure solution such as
-    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    # keep secrets safe.
-
-    instance_connection_name = 'adding-and-q-281-3662a64f:us-central1:flaskdb'  # e.g. 'project:region:instance'
-    db_user = 'sadmin'  # e.g. 'my-db-user'
-    db_pass = '10231993'  # e.g. 'my-db-password'
-    db_name = 'mdata'  # e.g. 'my-database'
-
-    ip_type = IPTypes.PUBLIC
-
-    connector = Connector(ip_type)
-
-    connect_args = {}
-    # If your SQL Server instance requires SSL, you need to download the CA
-    # certificate for your instance and include cafile={path to downloaded
-    # certificate} and validate_host=False. This is a workaround for a known issue.
-
-    def getconn() -> pytds.Connection:
-        conn = connector.connect(
-            instance_connection_name,
-            "pytds",
-            user=db_user,
-            password=db_pass,
-            db=db_name
-        )
-        return conn
-
-    pool = sqlalchemy.create_engine(
-        "mssql+pytds://",
-        creator=getconn,
-        # ...
-    )
-    return pool
-
-# db_user = 'sadmin'
-# db_password = '10231993'
-# db_name = 'mdata'
-# db_socket_dir = '/cloudsql'
-# cloud_sql_connection_name = 'adding-and-q-281-3662a64f:us-central1:flaskdb'
-#
-# # Configure the SQLAlchemy URI for Google Cloud SQL
-# db_uri = f"mssql://{db_user}:{db_password}@/{db_name}?unix_socket=/cloudsql/{cloud_sql_connection_name}"
-# app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#
-# # db = SQLAlchemy(app)
-# db = connect_unix_socket()
-
-db=connect_with_connector()
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-
-db.create_all()
-
-if not User.query.first():
-    sample_user = User(username='Sample User')
-    db.session.add(sample_user)
-    db.session.commit()
+# User ORM for SQLAlchemy
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
 
 
+@app.route('/add', methods=['POST'])
+def add():
+    # getting name and email
+    name = request.form.get('name')
+    email = request.form.get('email')
 
+    # checking if user already exists
+    user = Users.query.filter_by(email=email).first()
 
+    if not user:
+        try:
+            # creating Users object
+            user = Users(
+                name=name,
+                email=email
+            )
+            # adding the fields to users table
+            db.session.add(user)
+            db.session.commit()
+            # response
+            responseObject = {
+                'status': 'success',
+                'message': 'Successfully registered.'
+            }
 
-@app.route('/')
-def index():
-    # data=main.get_reviews('https://www.amazon.in/product-reviews/B077BFH786/&reviewerType=all_reviews/ref=cm_cr_arp_d_viewpnt_rgt?filterByStar=critical&pageNumber=1')
-    user = User.query.first()
+            return make_response(responseObject, 200)
+        except:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Some error occurred !!'
+            }
 
-    if user:
-        username = user.username
+            return make_response(responseObject, 400)
+
     else:
-        username = 'No user found'
+        # if user already exists then send status as fail
+        responseObject = {
+            'status': 'fail',
+            'message': 'User already exists !!'
+        }
 
-    return username
+        return make_response(responseObject, 403)
 
 
-if __name__=='__main__':
-    app.run(debug=False)
+@app.route('/view')
+def view():
+    # fetches all the users
+    users = Users.query.all()
+    # response list consisting user details
+    response = list()
+
+    for user in users:
+        response.append({
+            "name": user.name,
+            "email": user.email
+        })
+
+    return make_response({
+        'status': 'success',
+        'message': response
+    }, 200)
+
+
+if __name__ == "__main__":
+    # serving the app directly
+    app.debug=True
+    app.run()
